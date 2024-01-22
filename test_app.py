7281aa17ca01857791b482cac1be1059149eda68
webcoderz <19884161+webcoderz@users.noreply.github.com>
2024-01-22 13:59:12 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,8 +4,10 @@ from web3modal import connectComponent, disconnectComponent
 def app():
     st.title('My App')
     connect_button = st.connect_component()
-    if connect_button:
-        st.write(connect_button['address'])
+
+    if isinstance(connect_button, dict) and "address" in connect_button:
+        st.write(f'address: {connect_button["address"]}')
+
 
     if connect_button['address']:
         st.disconnect_component()
